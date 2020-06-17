9d18bea43cec89d23624d7d801796f253606f350
macarena chang <changmacarena@gmail.com>
2020-06-17 13:12:00 -0300

    correccion atributos

--- src/main/java/ar/com/ada/api/rrhh/entities/Empleado.java
+++ src/main/java/ar/com/ada/api/rrhh/entities/Empleado.java
@@ -1,13 +1,10 @@
 package ar.com.ada.api.rrhh.entities;
-
 import java.math.BigDecimal;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.*;
-
 import org.hibernate.annotations.NaturalId;
 
 
@@ -24,10 +21,74 @@ public class Empleado {
     private String nombre; 
     private int edad; 
     private BigDecimal sueldo;
-    private int estado; 
-    
-    private Categoria categoriaId; 
-    //faltan 3
+    private int estado;     
+    private Categoria categoria ;  
+    private Date fechaAlta;
+    private Date fechaBaja;
+
+    public int getEmpleadoId() {
+        return empleadoId;
+    }
+
+    public void setEmpleadoId(int empleadoId) {
+        this.empleadoId = empleadoId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public BigDecimal getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(BigDecimal sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Date getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public void setFechaBaja(Date fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
 
 
 
