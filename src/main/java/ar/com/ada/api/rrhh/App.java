ba24b1876152fb03a60bb9e0913f284fc21fbaca
macarena chang <changmacarena@gmail.com>
2021-05-27 04:49:03 -0300

    Add Swagger Documentation

--- src/main/java/ar/com/ada/api/rrhh/App.java
+++ src/main/java/ar/com/ada/api/rrhh/App.java
@@ -2,8 +2,8 @@ package ar.com.ada.api.rrhh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+
 public class App {
 
 	public static void main(String[] args) {
